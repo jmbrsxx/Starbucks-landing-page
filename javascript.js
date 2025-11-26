b7b6d6d5e174cf8cf7d70c29900a10016b9395b3
João Mateus <jmsrs375@gmail.com>
2025-11-25 23:24:24 -0300

    your message here

--- javascript.js
+++ javascript.js
@@ -7,3 +7,15 @@ closeTabBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-bars');
     icon.classList.toggle('fa-times');
 });
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+    if(window.scrollY > 0) {
+        header.style.position = "fixed";
+        header.style.width = "100%";
+        header.style.boxShadow = "0 4px 8px rgba(0, 0, 0, 0.2)";
+    }else{
+        header.style.position = "sticky";
+        header.style.boxShadow = "none";
+    }
+})
